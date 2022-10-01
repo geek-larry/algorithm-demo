1c07c74dc82430012a6405fd23647b423632def2
songyx <2250794569@qq.com>
2022-10-01 22:02:25 +0800

    最小堆

---
+++ heap/src/main/java/com/jade/heap/HeapApplication.java
@@ -0,0 +1,13 @@
+package com.jade.heap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeapApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HeapApplication.class, args);
+    }
+
+}
